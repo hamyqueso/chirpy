28cf3bdd5ee5fc30a00310e1d68ffc4709bcaf99
hamyqueso <jonathan.cheung09@gmail.com>
2025-09-18 14:37:34 -0700

    added a readiness endpoint /healthz and updated the file server path to /app

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
