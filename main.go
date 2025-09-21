f1b7d74a1b62e8a1722af1fbd5ea90994c63254f
hamyqueso <hamyqueso@gmail.com>
2025-09-21 14:21:57 -0700

    added a middleware that increments site hits. Working on endpoint that displays the hits and resets the hits.

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
-
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig {
+		fileserverHits: atomic.Int32{},
+	}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		(http.StripPrefix("/app", http.FileServer(http.Dir("."))))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request){
+		w.
 	})
 	server := &http.Server{
 		Handler: mux,
