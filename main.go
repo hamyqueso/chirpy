0af75b75ab721d2fc76d6e289b7474b0132a8f0e
hamyqueso <jonathan.cheung09@gmail.com>
2025-09-22 09:17:52 -0700

    finished hit count incrementation and added the hit count reset handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync/atomic"
 )
@@ -12,17 +13,20 @@ type apiConfig struct {
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	cfg.fileserverHits.Add(1)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
 func main() {
 	mux := http.NewServeMux()
-	apiCfg := apiConfig {
+	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
-		(http.StripPrefix("/app", http.FileServer(http.Dir("."))))))
+
+	handler := (http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.Handle("/app", apiCfg.middlewareMetricsInc(handler))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -30,8 +34,15 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request){
-		w.
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hits: %d", apiCfg.fileserverHits.Load())
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		apiCfg.fileserverHits.Store(0)
 	})
 	server := &http.Server{
 		Handler: mux,
